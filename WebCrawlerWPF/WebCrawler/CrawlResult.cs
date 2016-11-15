763a156b0a34e95b3885b47b33d5af4db19876fd
Dmitry <mr.blacky01@gmail.com>
2016-11-16 00:40:29 +0300

    + Add description of interface for crawling and crawlresult, add package htmlagilitypack, add constructor for crawler

---
+++ WebCrawlerWPF/WebCrawler/CrawlResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace WebCrawler
+{
+    public sealed class CrawlResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get or set dictionary of CrawResults
+        /// </summary>
+        public ConcurrentDictionary <string, CrawlResult> Urls { get; set; }
+
+        #endregion
+    }
+}
---
+++ WebCrawlerWPF/WebCrawler/Crawler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    public class Crawler : ISimpleWebCrawler
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get or set depth of crawling.
+        /// </summary>
+        private int CrawlingDepth
+        {
+            get { return CrawlingDepth; }
+            set
+            {
+                if(CheckRightDepth(value))
+                {
+                    CrawlingDepth = 6;
+                }
+                else
+                {
+                    CrawlingDepth = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Crawler Constructor.
+        /// </summary>
+        Crawler(int _depth, string[] _urls)
+        {
+
+        }
+        #endregion
+
+        #region Methods
+    
+        /// <summary>
+        /// Check is the depth right
+        /// </summary>
+        /// 
+        private bool CheckRightDepth(int depth)
+        {
+            if((depth < 1) ||(depth > 6))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        #endregion
+
+    }
+}
